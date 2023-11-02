5ffbe57c6f005cfaf72bbe9a06849e46d0c4e9d6
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-02 14:50:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+// function calculateMinCost() {
+//   //your code here
+  
+  
+  
+// }  
 function calculateMinCost() {
-  //your code here
+  let ropeLengths = document.getElementById('rope-lengths').value;
+  let lengths = ropeLengths.split(',').map(item => parseInt(item.trim(), 10));
   
+  let resultDiv = document.getElementById('result');
+  resultDiv.innerHTML = findMinCost(lengths);
+}
+
+function findMinCost(lengths) {
+  let minCost = 0;
   
+  while (lengths.length > 1) {
+    lengths.sort((a, b) => a - b);
+    let first = lengths.shift();
+    let second = lengths.shift();
+    
+    let sum = first + second;
+    minCost += sum;
+    lengths.push(sum);
+  }
   
-}  
+  return minCost;
+}
